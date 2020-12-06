023d2b70d422dd4f86aa9d3a6d0f0a569b2df8cf
Hiroaki Nishiuchi <nishiuchi.h.0414@gmail.com>
2020-12-06 18:02:32 +0900

    Add category api.

--- laravel_vue_quiz/resources/js/components/page/Home.vue
+++ laravel_vue_quiz/resources/js/components/page/Home.vue
@@ -17,23 +17,8 @@
               <img class="home-quiz__setting-h2-logo" src="/images/directory-icon.png" />出題設定
             </h2>
             <form>
-              <label>
-                <input type="checkbox" v-model="categories" value="1" />ビジネスマナー
-              </label>
-              <label>
-                <input type="checkbox" v-model="categories" value="2" />一般常識
-              </label>
-              <label>
-                <input type="checkbox" v-model="categories" value="3" />就職・転職
-              </label>
-              <label>
-                <input type="checkbox" v-model="categories" value="4" />法律
-              </label>
-              <label>
-                <input type="checkbox" v-model="categories" value="5" />IT
-              </label>
-              <label>
-                <input type="checkbox" v-model="categories" value="6" />雑学
+              <label v-for="(cate, index) in category" :key="index">
+                <input type="checkbox" v-model="categories" :value="cate.id" />{{cate.name}}&ensp;
               </label>
               <div class>
                 全項目チェック
@@ -98,12 +83,16 @@ export default {
   data(){
     return {
       categories: [1],
-      information: []
+      information: [],
+      category: [],
     }
   },
   mounted(){
     this.$http.get("/api/information").then(response => {
       this.information = response.data
+    })
+    this.$http.get("/api/category").then(response => {
+      this.category = response.data
     })
   },
   methods: {
